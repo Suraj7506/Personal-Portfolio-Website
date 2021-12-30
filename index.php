06030fb00056bd3079aeafba9f3aeda1ba9d0bab
Suraj Shukla <92645951+Suraj7506@users.noreply.github.com>
2021-12-30 16:11:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
+
 body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
 .w3-row-padding img {margin-bottom: 12px}
 /* Set the width of the sidebar to 120px */
@@ -23,7 +25,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
 <!-- Icon Bar (Sidebar - hidden on small screens) -->
 <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
   <!-- Avatar image in top left corner -->
-  <img src="myimage.png" style="width:100%">
+  <img src="myimage1.png" style="width:100%">
   <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
     <i class="fa fa-home w3-xxlarge"></i>
     <p>HOME</p>
@@ -32,7 +34,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
     <i class="fa fa-user w3-xxlarge"></i>
     <p>ABOUT</p>
   </a>
-  <a href="#Projects" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+  <a href="#projects" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
     <i class="fa fa-eye w3-xxlarge"></i>
     <p>Projects</p>
   </a>
@@ -47,7 +49,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
     <a href="#" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
     <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">ABOUT</a>
-    <a href="#photos" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
+    <a href="#projects" class="w3-bar-item w3-button" style="width:25% !important">Projects</a>
     <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
   </div>
 </div>
@@ -58,7 +60,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
     <h1 class="w3-jumbo"><span class="w3-hide-small"> Hii, I'm </span> Suraj Shukla.</h1>
     <p>UI/UX Developer and Web Designer.</p>
-    <img src="myimage.png" alt="boy" class="w3-image" width="400" height="450">
+    <img src="myimage1.png" alt="boy" class="w3-image" width="400" height="450">
   </header>
 
   <!-- About Section -->
@@ -170,7 +172,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   </div>
   
   <!-- Portfolio Section -->
-  <div class="w3-padding-64 w3-content" id="Projects">
+  <div class="w3-padding-64 w3-content" id="projects">
     <h2 class="w3-text-light-grey">Projects</h2>
     <hr style="width:200px" class="w3-opacity">
 
@@ -213,8 +215,8 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
       <p><input class="w3-input w3-padding-16" type="text" placeholder="Subject" required name="Subject"></p>
       <p><input class="w3-input w3-padding-16" type="text" placeholder="Message" required name="Message"></p>
       <p>
-        <button class="w3-button w3-light-grey w3-padding-large" type="submit">
-          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        <button class="w3-button w3-light-grey w3-padding-large" type="text">
+          <a href = "mailto: sdshukla270@gmail.com">Send Message</a>
         </button>
       </p>
     </form>
@@ -223,12 +225,15 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   
     <!-- Footer -->
   <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <a href="https://www.facebook.com/profile.php?id=100009028896282" class="fa fa-facebook"></a>
+<a href="https://twitter.com/SurajShukla21?s=20" class="fa fa-twitter"></a>
+<a href="https://www.yourquote.in/sd-shukla-bi00r/quotes" class="fa fa-google"></a>
+<a href="https://www.linkedin.com/in/suraj-shukla-748497203" class="fa fa-linkedin"></a>
+<a href="https://instagram.com/_suraj_shukla._?utm_medium=copy_link" class="fa fa-instagram"></a>
+<a href="https://in.pinterest.com/sdshukla270/_saved/" class="fa fa-pinterest"></a>
+<a href="https://github.com/Suraj7506" class="fa fa-github"></a>
+
+
     <p class="w3-medium">Designed by <a href="https://github.com/Suraj7506" target="_blank" class="w3-hover-text-green">Suraj Shukla </a></p>
   <!-- End footer -->
   </footer>
@@ -238,4 +243,3 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
 
 </body>
 </html>
-
